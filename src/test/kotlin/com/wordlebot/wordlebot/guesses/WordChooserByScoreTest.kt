5812e84db8884f3284eec9bc2bc1f2904a052406
erickbressani <erickbress@gmail.com>
2022-12-14 09:55:53 +0100

    add more tests to WordChooserByScoreTest

--- src/test/kotlin/com/wordlebot/wordlebot/guesses/WordChooserByScoreTest.kt
+++ src/test/kotlin/com/wordlebot/wordlebot/guesses/WordChooserByScoreTest.kt
@@ -5,24 +5,68 @@ import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
 internal class WordChooserByScoreTest {
+    private val chooser = WordChooserByScore()
+
     @Test
     fun `should choose word with highest score`() {
-        val chooser = WordChooserByScore()
-
-        val word = chooser.choseBasedOn(WORDS, WORDS)
-
-        word.value shouldBe "abaci"
-    }
-
-    companion object {
-        val WORDS = listOf(
+        val words = words(
             "aapas",
             "aargh",
             "aarti",
             "abaca",
             "abaci",
-            "abaht",
             "abode",
-        ).map(::Word)
+            "glare",
+            "yechy",
+            "yedes"
+        )
+
+        val (result) = chooser.choseBasedOn(words, words)
+
+        result shouldBe "abode"
     }
+
+    @Test
+    fun `should choose word with most repeatable pattern when a score tie is found`() {
+        val words = words(
+            "acode",
+            "acole",
+            "trode",
+            "grola"
+        )
+
+        val (result) = chooser.choseBasedOn(words, words)
+
+        result shouldBe "acode"
+    }
+
+    @Test
+    fun `should consider only matches when scoring chars`() {
+        val words = words(
+            "aapas",
+            "aargh",
+            "aarti",
+            "abaca",
+            "abaci",
+            "abode",
+            "glare",
+            "yechy",
+            "yedes"
+        )
+
+        val matches = words(
+            "aapas",
+            "aargh",
+            "aarti",
+            "abaca",
+            "abaci",
+        )
+
+        val (result) = chooser.choseBasedOn(words, matches)
+
+        result shouldBe "abaci"
+    }
+
+    private fun words(vararg words: String): List<Word> =
+        words.map(::Word)
 }
